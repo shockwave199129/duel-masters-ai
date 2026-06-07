1e97bc67ac5176ad50cdac2fc08b5d44f0f29424
subhrangshu <subhrangshu.m@aqbsolutions.com>
2026-06-07 23:29:44 +0530

    Avoid torch import in lightweight bot package

--- dm_engine/bot/__init__.py
+++ dm_engine/bot/__init__.py
@@ -1,16 +1,12 @@
 """Bot and AI-facing helpers for dm_engine."""
 
 from bot.action_encoder import ACTION_VECTOR_SIZE, encode_action
-from bot.neural_bot import NeuralBot
-from bot.neural_model import ActionScoreNet
 from bot.random_bot import RandomBot
 from bot.state_encoder import OBSERVATION_VECTOR_SIZE, encode_observation
 
 __all__ = [
     "ACTION_VECTOR_SIZE",
     "OBSERVATION_VECTOR_SIZE",
-    "ActionScoreNet",
-    "NeuralBot",
     "RandomBot",
     "encode_action",
     "encode_observation",
